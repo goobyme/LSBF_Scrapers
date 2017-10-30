90df5b5e8857692f633b06f0e1595a34fa84c19a
James Lo <goobyme@gmail.com>
2017-10-30 14:35:58 -0700

    10/30 2:35

---
+++ BIg.py
@@ -0,0 +1,6 @@
+import time, datetime
+
+print(datetime.datetime.date(s))
+
+
+
---
+++ Test.py
@@ -0,0 +1,21 @@
+import requests, os
+
+
+
+class ScraperShell:
+
+    def __init__(self, url):
+
+       url = url.self
+
+
+
+
+class PictureScraper(ScraperShell):
+
+    pass
+
+
+class TextScraper(ScraperShell):
+
+    pass
