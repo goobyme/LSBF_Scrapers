f1acfa6c0e2adc30a180a6c559aa77dabe0879e7
James Lo <goobyme@gmail.com>
2017-11-01 15:53:17 -0700

    10/30 Post work

--- BIg.py
+++ BIg.py
@@ -1,6 +1,15 @@
-import time, datetime, bs4
+import bs4
+import urllib
 
-print(datetime.datetime.date())
+
+BK_PAGE = "https://www.berkadia.com/people-and-locations/people/"
+
+
+
+with urllib.request.urlopen(BK_PAGE) as page:
+    soup = bs4.BeautifulSoup(page.content(), 'lxml').find_all('div', {'class': 'col-xs-9 col-sm-9 col-md-10'})
+    for link in [x.a['href'] for x in soup.find_all('h2')]:
+        print(link)
 
 
 
