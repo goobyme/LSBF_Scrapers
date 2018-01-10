e9e754bd4b3b143366d36b6499588628f38fcfd9
James Lo <goobyme@gmail.com>
2018-01-09 22:57:55 -0800

    Edit for broken laptop

--- Scrapy_Scrapers/ScottsmanYard_Scraper/scottsman.py
+++ Scrapy_Scrapers/ScottsmanYard_Scraper/scottsman.py
@@ -16,31 +16,44 @@ class ScottSpider(scrapy.Spider):
 
     def parse_details(self, response):
         self.log('Scraping Detail Page {}...'.format(response.url))
-        idregex = re.compile(r"(?<==)\d$")
+        idregex = re.compile(r"(?<==)\d+$")
         lend_id = idregex.findall(response.url)[0]
+        to_splice = response.css('td.widehalf_FWSNAME::text').extract()
+        spliced = to_splice[1].split(',')
 
         company = {
             'Company Name': response.css('td.widehalf_FWSNAME > span > strong::text').extract_first(),
             'Company Email': response.css('td.widehalf_FWSNAME > a[title="Lender Email Address"]::text').extract_first(),
-            'Company Website': response.css('td.widehalf_FWSNAME > a#LenderDetail_hreWeb').extract_first(),
-            'Street Address': response.css('td.widehalf_FWSNAME'),
-            'Comments': response.css('td[colspan="2"]::text').extract_first(),
-            'Finance URL': 'http://www.scotsmanguide.com/rsPopDefault.aspx?ucAdd=1001&MTXID=49&LenderId=' + str(lend_id)
+            'Company Website': response.css('td.widehalf_FWSNAME > a#LenderDetail_hreWeb::attr(href)').extract_first(),
+            'Company Phone': to_splice[2].replace('Phone:', ''),
+            'Street Address': to_splice[0],
+            'Comments': response.css('td[colspan="2"]::text').extract()[1],
+            'City': spliced[0].strip(),
+            'State': spliced[1].strip(),
+            'Postal Code': spliced[2].strip(),
         }
-        fi_inf = scrapy.Request(url=company.setdefault('Finance URL'), callback=self.parse_finance)
-        company.update(fi_inf)
+
+        matrix_id = response.css('img.imMatrix::attr(title)').extract()
+        ref = {'Commercial': '49', 'Hard Money':'51', 'Multifamily': '50', 'Construction': '52'}
+        for item in matrix_id:
+            parse_id = ref.setdefault(item, '49')
+            company['Finance URL_{}'.format(item)] = """
+                    http://www.scotsmanguide.com/rsPopDefault.aspx?ucAdd=1001&MTXID={}&LenderId={}
+                    """.format(parse_id, lend_id)
+
+        scrapy.Request(url=company.setdefault('Finance URL'), callback=self.parse_finance)
 
         yield company
 
-        employee_table = response.css('table.gColmCPHmain2Content1')
-        for emp in employee_table.css('td.widehalf'):
+        for emp_el in response.css('tr > td[colspan="2"] > table.gColmCPHmain2Content1 td.widehalf'):
             yield {
-                'Name': emp.css('b::text').extract_first(),
-                'Email': emp.css('a::text').extract_first(),
-                'Phone': emp.css('::text').extract_first()
+                'Name': emp_el.css('b::text').extract_first(),
+                'Email': emp_el.css('a::text').extract_first(),
+                'Phone': emp_el.css('::text').extract()
             }
 
-    def parse_finance(self, response):
+    @staticmethod
+    def parse_finance(response):
         loantypes = []
         property_types = []
         e_list = response.css('td.LSRbody1CwtextC')
@@ -56,10 +69,12 @@ class ScottSpider(scrapy.Spider):
                 continue
 
         yield {
-            'Min': response.css('abbr[title="Min $"::text').extract_first(),
-            'Max': response.css('abbr[title="Max $"::text').extract_first(),
-            'LTV': response.css('abbr[title="LTV"::text').extract_first(),
-            'DSCR': response.css('abbr[title="Debt Service Coverage Ratio"::text').extract_first(),
+            'Min': response.css('abbr[title="Min $"]::text').extract_first(),
+            'Max': response.css('abbr[title="Max $"]::text').extract_first(),
+            'LTV': response.css('abbr[title="LTV"]::text').extract_first(),
+            'DSCR': response.css('abbr[title="Debt Service Coverage Ratio"]::text').extract_first(),
+            'Firm Type': response.css('td[colspan="4"]::text').extract_first(),
+            "Covered Regions": response.css('td[colspan="34"] > span::text').extract_first(),
             'Loan Types': loantypes,
             'Property Types': property_types
         }
