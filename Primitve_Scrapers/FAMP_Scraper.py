d9ef8e7d0fa523d25cb4104c2170b30bed199b44
goobyme <Computer8>
2018-01-31 11:46:36 -0800

    FAMP

---
+++ Primitve_Scrapers/FAMP_Scraper.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium import common
+import shelve
+import time
+
+
+initpage = "http://www.myfamp.org/directory#/"
+
+browser = webdriver.Firefox()
+browser.get(initpage)
+
+browser.find_element_by_css_selector('a.gen-button').click()
+time.sleep(2)
+
+
+def find_element(css_selector, element, retries=3):
+    for l in range(retries):
+        try:
+            return element.find_element_by_css_selector(css_selector)
+        except common.exceptions.NoSuchElementException:
+            return None
+        except common.exceptions.StaleElementReferenceException:
+            continue
+
+
+links = []
+page_count = browser.find_element_by_css_selector('strong#total-pages').text
+for i in range(int(page_count)):
+    profile_elements_count = len(browser.find_elements_by_css_selector('div.search-profile.ng-scope'))
+    for x in range(profile_elements_count):
+        profile_elements = browser.find_elements_by_css_selector('div.search-profile.ng-scope')
+        profile_elements[x].click()
+        time.sleep(2)
+        links.append(browser.current_url)
+        print('Appended {}'.format(browser.current_url))
+        find_element('a#back-to-search', browser).click()
+        time.sleep(2)
+    if i != 93:
+        browser.find_element_by_css_selector('a#next').click()
+
+
+with shelve.open('FAMP_Links') as shelf_file:
+    shelf_file['links'] = links
